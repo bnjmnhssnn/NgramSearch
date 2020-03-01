61995a4fba6f0e9b0dac2982de005b956c532bfc
bnjmnhssnn <info@benjamin-hosseinian.de>
2020-03-01 13:58:02 +0000

    Request Validierung in separate Funktion verlagert

--- src/request_handlers/create_index.php
+++ src/request_handlers/create_index.php
@@ -1,39 +1,11 @@
 <?php
 function create_index(array $vars = [], stdClass $payload) : void {
 
-    $index_name_max_strlen = 12;
+    if(!validate_request($payload)) {
+        return;
+    }
     $storage = get_storage_adapter();
 
-    if(empty($payload->index_name)) {
-        set_header("HTTP/1.1 400 Bad Request"); 
-        set_header('Content-type: application/json');
-        echo json_encode(
-            [
-                'msg' => '$_POST[\'index_name\'] is a required parameter and must not be missing or empty.'
-            ]
-        ); 
-        return;     
-    }
-    if(preg_match( '/[^a-zA-Z0-9_-]+/', $payload->index_name)) {
-        set_header("HTTP/1.1 400 Bad Request");
-        set_header('Content-type: application/json');
-        echo json_encode(
-            [
-                'msg' => 'The index name contains invalid chars.'
-            ]
-        ); 
-        return; 
-    }
-    if(strlen($payload->index_name) > $index_name_max_strlen) {
-        set_header("HTTP/1.1 400 Bad Request"); 
-        set_header('Content-type: application/json');
-        echo json_encode(
-            [
-                'msg' => 'The index name exeeds the allowed maximum of ' . $index_name_max_strlen . ' chars.'
-            ]
-        ); 
-        return;  
-    }
     if(!$storage->createIndex($payload->index_name)) {
         switch($storage->lastError()) {
             case $storage::ERROR_INDEX_NAME_INUSE:
@@ -58,4 +30,33 @@ function create_index(array $vars = [], stdClass $payload) : void {
     ); 
     return; 
     
+}
+
+function validate_request(stdClass $payload) : bool
+{
+    $index_name_max_strlen = 12;
+    $validation_errors = [];
+
+    if(empty($payload->index_name)) {
+        $validation_errors[] = '$_POST[\'index_name\'] is a required parameter and must not be missing or empty.';  
+    } else {
+        if(preg_match( '/[^a-zA-Z0-9_-]+/', $payload->index_name)) {
+            $validation_errors[] = 'The index name contains invalid chars.';
+        }
+        if(strlen($payload->index_name) > $index_name_max_strlen) {
+            $validation_errors[] = 'The index name exeeds the allowed maximum of ' . $index_name_max_strlen . ' chars.';
+        }
+    }
+    if (!empty($validation_errors)) {
+        set_header("HTTP/1.1 400 Bad Request"); 
+        set_header('Content-type: application/json');
+        echo json_encode(
+            [
+                'msg' => 'The validation of the request has failed.',
+                'validation_errors' => $validation_errors
+            ]
+        ); 
+        return false;
+    }
+    return true;
 }
