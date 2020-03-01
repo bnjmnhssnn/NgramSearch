e4447eed8f6cdf86d5b3b196ec02ed4defec1ce9
bnjmnhssnn <info@benjamin-hosseinian.de>
2020-03-01 12:53:16 +0000

    Pfadendungs-Schrägstrich Problematik gefixt

--- src/classes/StorageAdapter/Filesystem.php
+++ src/classes/StorageAdapter/Filesystem.php
@@ -5,12 +5,17 @@ class Filesystem implements StorageAdapterInterface
 {
     protected $last_error;
 
+    const ERROR_INDEX_NAME_INUSE = 0;
+    const ERROR_CREATE_INDEX = 1;
+    const ERROR_INDEX_NOT_FOUND = 2;
+    const ERROR_DROP_INDEX = 3;
+
     public function __construct(string $storage_path)
     {
         if(!is_dir($storage_path)) {
             throw new \InvalidArgumentException($storage_path . ' is not a directory.');
         }
-        $this->storage_path = $storage_path;
+        $this->storage_path = rtrim($storage_path, '/');
     }
 
     public function listIndexes() : array
@@ -18,19 +23,21 @@ class Filesystem implements StorageAdapterInterface
         return array_filter(
             scandir($this->storage_path),
             function($item) {
-                return (is_dir($this->storage_path . $item) && !in_array($item, ['.', '..']));
+                return (is_dir($this->storage_path . '/' . $item) && !in_array($item, ['.', '..']));
             }
         );
     }  
     
     public function createIndex(string $name) : bool
     {
-        if (file_exists($this->storage_path . $name)) {
-            $this->last_error = 'Index Name \'' . $name . '\' already in use.'; 
+        if (file_exists($this->storage_path . '/' . $name)) {
+            #$this->last_error = 'Index Name \'' . $name . '\' already in use.'; 
+            $this->last_error = self::ERROR_INDEX_NAME_INUSE; 
             return false;
         }
-        if(!mkdir($this->storage_path . $name, 0777, true)) {
-            $this->last_error = 'Error creating Index \'' . $name . '\'.'; 
+        if(!mkdir($this->storage_path . '/' . $name, 0777, true)) {
+            #$this->last_error = 'Error creating Index \'' . $name . '\'.';
+            $this->last_error = self::ERROR_CREATE_INDEX; 
             return false; 
         }
         return true;
@@ -38,12 +45,14 @@ class Filesystem implements StorageAdapterInterface
     
     public function dropIndex(string $name) : bool
     {
-        if (!file_exists($this->storage_path . $name) || !is_dir($this->storage_path . $name)) {
-            $this->last_error = 'Index \'' . $name . '\' not found.'; 
+        if (!file_exists($this->storage_path . '/' . $name) || !is_dir($this->storage_path . '/' . $name)) {
+            #$this->last_error = 'Index \'' . $name . '\' not found.'; 
+            $this->last_error = self::ERROR_INDEX_NOT_FOUND; 
             return false;
         }
-        if(!$this->rrmdir($this->storage_path . $name)) {
-            $this->last_error = 'Error while removing Index \'' . $name . '\'.'; 
+        if(!$this->rrmdir($this->storage_path . '/' . $name)) {
+            #$this->last_error = 'Error while removing Index \'' . $name . '\'.'; 
+            $this->last_error = self::ERROR_DROP_INDEX; 
             return false;      
         }
         return true;
